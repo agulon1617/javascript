98ae46fdbef0889225b75eeaee9b339fe6447249
agulon1617 <agulon@gmail.com>
2021-04-20 15:21:45 +0200

    31-js-zdarzenie-onclick

---
+++ onclick/main.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('button');
+let myHeading =document.querySelector('button');
+myHeading.textContent = 'Klik!';
+myHeading.onclick = function() {
+alert("klik dziala<button>");
+}
+
+console.log(myHeading);
